d2b307c92f5c886e105cc0c3d3392dcb2266049a
Kevin Svitač <kevin@svit.ac>
2025-05-11 08:33:45 +0200

    konvertovanie obrázkov v kvízoch na blob

--- poznamkovac.ts
+++ poznamkovac.ts
@@ -1,7 +1,9 @@
 import vykreslitMapu from "./src/PojmovaMapa";
 import ocislujPojmy from "./src/PojmyOcislovanie";
+import nahradKvizObrazkyAkoBlob from "./src/KvizBlob";
 
 globalThis.addEventListener("load", () => {
     ocislujPojmy();
     vykreslitMapu();
+    nahradKvizObrazkyAkoBlob();
 });
---
+++ src/KvizBlob.ts
@@ -0,0 +1,18 @@
+export default function nahradKvizObrazkyAkoBlob() {
+    const kvizSubory = globalThis.document.querySelectorAll('.quiz .mw-default-size[typeof="mw:File"]');
+
+    kvizSubory.forEach(async (subor) => {
+        const obrazok = subor.querySelector("img");
+        if (!obrazok) return;
+
+        const obrazokUrl = obrazok.src;
+        const blob = await fetch(obrazokUrl).then((r) => r.blob());
+        obrazok.src = URL.createObjectURL(blob);
+
+        const a = subor.querySelector("a");
+        if (!a) return;
+
+        a.href = "#";
+        a.dataset.bsTitle = "Súbor:?";
+    });
+}
